ad162591c2b5ad4abdb00375d7f4561ced82e3bb
mason <58895787+masoniis@users.noreply.github.com>
2025-05-16 10:08:18 -0600

    initial plugin setup

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Development environment for an Obsidian plugin using Bun";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            bun
+          ];
+        };
+      }
+    );
+}
